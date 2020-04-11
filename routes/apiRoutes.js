8819bd5a7e0ec0a61fa96697f73ba78e122a539f
PROX-E <lichlord18@gmail.com>
2020-04-11 13:23:37 -0600

    update api req

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,4 +20,18 @@ module.exports = function (app) {
                 res.json(err)
             })
     });
+
+    
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
 };

5b60d92bc48aa0f1034b4435dbe4727301d29d6e
PROX-E <lichlord18@gmail.com>
2020-04-11 13:10:35 -0600

    update server.js
    add express func's to handle parsing data

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(morgan("dev"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));

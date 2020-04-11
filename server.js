58ff4ed739320251bf7f8885f00351c65892ea69
PROX-E <lichlord18@gmail.com>
2020-04-11 13:09:16 -0600

    express app setup
    & create port

--- server.js
+++ server.js
@@ -2,3 +2,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+
+// Setting up Express App
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(morgan("dev"));
